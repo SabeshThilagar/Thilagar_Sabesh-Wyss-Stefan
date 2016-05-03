7a9bb15057abda5c87837dd0acc2e023a1c86124
swyss <stefanwysspc@gmail.com>
2016-05-03 18:10:13 +0200

    Added files via upload

---
+++ NewMain.java
@@ -0,0 +1,19 @@
+/*
+$FHNW
+ */
+package Model;
+
+/**
+ *
+ * @author Stefan
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
