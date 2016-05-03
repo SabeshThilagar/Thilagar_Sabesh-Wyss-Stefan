35734ef32301adbc54b70cb7f95a4fd8260a9357
swyss <stefanwysspc@gmail.com>
2016-05-03 18:19:12 +0200

    Added files via upload

---
+++ Mainview.java
@@ -0,0 +1,19 @@
+/*
+$FHNW
+ */
+package View;
+
+/**
+ *
+ * @author Stefan
+ */
+public class Mainview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
