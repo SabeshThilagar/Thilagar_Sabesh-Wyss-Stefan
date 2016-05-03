052baa3e14c9b340659d01a8e30e0f1229dcdceb
Stefan Wyss <stefan.wyss3@students.fhnw.ch>
2016-05-03 18:19:12 +0200

    Added files via upload

---
+++ JavaApplication8/src/View/MainView.java
@@ -0,0 +1,19 @@
+/*
+$FHNW
+ */
+package View;
+
+/**
+ *
+ * @author Stefan
+ */
+public class MainView {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
