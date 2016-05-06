34b0ba93f89b4ff59530c5aad3786b4bf0f0a9e1
Stefan Wyss <stefan.wyss3@students.fhnw.ch>
2016-05-06 12:46:16 +0200

    Merge branch 'master' of https://github.com/swyss/Thilagar_Sabesh-Wyss-Stefan
    # Conflicts:
    #	Thilagar_Sabesh-Wyss_Stefan/src/Model/FileBackend.java
    #	Thilagar_Sabesh-Wyss_Stefan/src/Presenter/MainPresenter.java
    #	Thilagar_Sabesh-Wyss_Stefan/src/View/MainView.java
    #	Thilagar_Sabesh-Wyss_Stefan/src/View/ViewUI.java

--- Thilagar_Sabesh-Wyss_Stefan/src/Model/FileBackend.java
+++ Thilagar_Sabesh-Wyss_Stefan/src/Model/FileBackend.java
@@ -1,9 +1,13 @@
 package Model;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,24 +19,27 @@ import java.util.List;
 public class FileBackend implements MainModel {
 
     private List<Film> data;
-    private String fileName = "movies.txt"; //Standard-FILE
+    private String fileName = "movies.csv"; //Standard-FILE
+
     private final List<Film> eData = new ArrayList<>();
 
     @Override
     public List<Film> getData() {
         URL url = this.getClass().getResource(fileName);
-        System.out.println(url); //TEST
+        
+        System.out.println(url);
         if (url != null) {
-            System.out.println(fileName);
+
             if (".txt".equals(url.toString().substring(url.toString().length() - 4))) {
+                System.out.println("txt" + fileName);
                 return getDataTXT(getClass().getResourceAsStream(fileName));
             } else if (".csv".equals(url.toString().substring(url.toString().length() - 4))) {
-                return getDataCSV(getClass().getResourceAsStream(fileName));
+                System.out.println("csv" + fileName);
+                return getDataCSV(fileName);
             } else {
                 System.out.println("kein unterstütztes File");//TEST
                 return eData;
             }
-
         } else {
             System.out.println("kein File");//TEST
             return eData;
@@ -40,7 +47,8 @@ public class FileBackend implements MainModel {
     }
 
     @Override
-    public void setFileName(String name) {
+    public void setFileName(String name
+    ) {
         this.fileName = name;
     }
 
@@ -75,41 +83,32 @@ public class FileBackend implements MainModel {
             System.out.println("General I/O exception: " + exception.getMessage());
         }
         return data;
-
     }
 
-    public List<Film> getDataCSV(InputStream iStream) {
+    public List<Film> getDataCSV(String fn) {
+        String zeile;
+        String[] split = null;
+        ArrayList list = new ArrayList();
         data = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(iStream))) {
-            bufferedReader.lines().skip(1).forEach(e -> {
-                String[] col = e.split(";");
-                data.add(new Film(
-                        Integer.parseInt(col[0]), //ID
-                        col[1], //TITLE
-                        Integer.parseInt(col[2]), //YEARofAWARD
-                        col[3], //DIRECTOR
-                        col[4], //MAINACTOR
-                        col[5], //TITLEeng
-                        Integer.parseInt(col[6]), //YEARofPRODUCTION
-                        col[7], //COUNTRY
-                        Integer.parseInt(col[8]), //DURATION
-                        Integer.parseInt(col[9]), //FSK
-                        col[10], //GENRE
-                        col[11], //STARTDate
-                        Integer.parseInt(col[12]))); //NUMBERofOSKARS
-            });
-//TEST
-            data.stream().forEach((f) -> {
-                System.out.println(f.getTitle());
-            });
-//TEST
 
+        try {
+            FileReader fileR = new FileReader(fn);
+            BufferedReader buff = new BufferedReader(fileR);
+            while ((zeile = buff.readLine()) != null) {
+                split = zeile.split(";");
+                for (int i = 0; i < split.length; i++) {
+                    //leere Zeilen ignorieren
+                    if (!(split[i].equals(""))) //eventuelle Leerzeichen zwischen zwei ';' entfernen
+                    //und Wert in Liste schreiben
+                    {
+                        list.add(split[i].trim());
+                    }
+                }
+            }
         } catch (IOException exception) {
-            System.out.println("falsche Daten");
+            System.out.println("falsche Daten" + fn);
             System.out.println("General I/O exception: " + exception.getMessage());
         }
         return data;
-
     }
-
 }
